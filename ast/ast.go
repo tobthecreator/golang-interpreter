8f5cc9e1e54c059fa28ef083a8af412166c296e3
Tyler O'Briant <tylerobriant@gmail.com>
2020-11-19 11:55:50 -0600

    First pass at the AST

---
+++ ast/ast.go
@@ -0,0 +1,47 @@
+package ast
+
+import "github.com/tobthecreator/golang-interpreter/token"
+
+// Everything in the AST must implement Node
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // source token.Token object
+	Name  *Identifier // the x in "let x = 5"
+	Value Expression  // the 5 in "let x = 5"
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
