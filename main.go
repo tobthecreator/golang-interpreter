b8a51207f2c4789c41fb68dab15d4e4c637b7d84
Tyler O'Briant <tylerobriant@gmail.com>
2020-11-18 21:26:42 -0600

    Adding repl to main func run

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/tobthecreator/golang-interpreter/repl"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the TAWB programming language\n", user.Username)
+
+	repl.Start(os.Stdin, os.Stdout)
 }
