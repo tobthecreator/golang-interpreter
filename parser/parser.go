98c005e2fb785757072b593e16dd5780983021cc
Tyler O'Briant <tylerobriant@gmail.com>
2020-11-20 19:41:45 -0600

    Pseudocode setup

--- parser/parser.go
+++ parser/parser.go
@@ -1,3 +1,5 @@
+package parser
+
 import (
 	"github.com/tobthecreator/golang-interpreter/ast"
 	"github.com/tobthecreator/golang-interpreter/lexer"
@@ -27,4 +29,82 @@ func (p *Parser) nextToken() {
 
 func (p *Parser) ParseProgram() *ast.Program {
 	return nil
+	// program = newProgramASTNode()
+
+	// advanceTokens()
+
+	// for currentToken() != EOF_TOKEN {
+	// 	statement = null
+
+	// 	if currentToken() == LET_TOKEN {
+	// 		statement = parseLetStatement()
+	// 	} else if currentToken() == RETURN_TOKEN {
+	// 		statement = parseReturnStatement()
+	// 	} else if currentToken() == IF_TOKEN {
+	// 		statement = parseIfStatement()
+	// 	}
+
+	// 	if statement != null {
+	// 		program.Statements.push(statement)
+	// 	}
+
+	// 	advanceTokens()
+	// }
+
+	// return program
 }
+
+// Pseudo-code we're going for
+// function parseLetStatement() {
+// 	advanceTokens()
+
+// 	identifier = parseIdentifier()
+
+// 	advanceTokens()
+
+// 	if currentToken() != EQUAL_TOKEN {
+// 		parseError("no equal sign")
+// 		return null
+// 	}
+
+// 	advanceTokens()
+
+// 	value = parseExpression()
+
+// 	variableStatement = newVariableStatementASTNode()
+// 	variableStatement.identifier = identifier
+// 	variableStatement.value = value
+
+// 	return variableStatement
+// }
+
+// function parseIdentifier() {
+// 	identifier = newIdentifierASTNode()
+// 	identifier.token = currentToken()
+
+// 	return identifier
+// }
+
+// function parseExpression() {
+// 	if (currentToken() == INTERGER_TOKEN) {
+// 		if (nextToken() == PLUS_TOKEN) {
+// 			return parseOperationExpression()
+// 		} else if (nextToken() == SEMICOLON_TOKEN) {
+// 			return parseIntegerLiteral()
+// 		}
+// 	} else if (currentToken() == LEFT_PAREN) {
+// 		return parsedGroupedExpression()
+// 	}
+// }
+
+// function parseOperatorExpression() {
+// 	operatorExpression = newOperatorExpression()
+
+// 	operatorExpression.left = parseIntegerLiteral()
+// 	advanceTokens()
+// 	operatorExpression.operator = currentToken()
+// 	advanceTokens()
+// 	operatorExpression.right = parseExpression()
+
+// 	return operatorExpression
+// }
